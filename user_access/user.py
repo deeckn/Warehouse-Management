54a1e5b5a998f6e466891d0dd770c30ae6fa6292
deeckn <dee28511@gmail.com>
2022-01-30 17:19:57 +0700

    Add getter for password

--- user_access/user.py
+++ user_access/user.py
@@ -4,11 +4,14 @@ from user_access.access_level import AccessLevel
 class User:
     def __init__(self, username: str, password: str, access: AccessLevel) -> None:
         self.username = username
-        self.__password = password
+        self.password = password
         self.access = access
 
     def get_username(self) -> str:
         return self.username
+
+    def get_password(self) -> str:
+        return self.password
 
     def get_access_level(self) -> AccessLevel:
         return self.access

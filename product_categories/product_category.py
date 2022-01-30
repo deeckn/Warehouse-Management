0a90af2cdbab84c97e79c7fc747af66b54c03508
deeckn <dee28511@gmail.com>
2022-01-31 01:07:57 +0700

    Removed redundant file

--- product_categories/product_category.py
+++
@@ -1,8 +0,0 @@
-
-class ProductCategory:
-    def __init__(self, category_name: str) -> None:
-        super().__init__()
-        self.category_name = category_name
-
-    def get_category_name(self) -> str:
-        return self.category_name

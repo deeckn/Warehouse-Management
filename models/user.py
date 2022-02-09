61f2721ac6a07f15cc077ab6ac3c272231798c83
deeckn <dee28511@gmail.com>
2022-02-09 11:46:58 +0700

    Add first and last name attribute

--- models/user.py
+++ models/user.py
@@ -5,12 +5,20 @@ from access_level import AccessLevel
 @dataclass
 class User:
     __id: int
+    __first_name: str
+    __last_name: str
     __username: str
     __password: str
     __access: AccessLevel
 
     def get_id(self) -> int:
         return self.__id
+
+    def get_first_name(self) -> str:
+        return self.__first_name
+
+    def get_last_name(self) -> str:
+        return self.__last_name
 
     def get_username(self) -> str:
         return self.__username

f4609db55efa828ff7e916580ef0c4599c0d453e
deeckn <dee28511@gmail.com>
2022-02-09 11:43:37 +0700

    Add insert log entry method

--- models/app_dao.py
+++ models/app_dao.py
@@ -2,24 +2,25 @@ import sqlite3
 import os.path
 from user_dao import UserDAO
 from customer_dao import CustomerDAO
-from room_dao import RoomDAO
+from product_dao import ProductDAO
 from shelf_dao import ShelfDAO
 from log_dao import LogDAO
 from user import User
+from log_entry import LogEntry
 
 
 class AppDAO:
 
     __BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    __db_path = os.path.join(__BASE_DIR, "geb_arai_dee.db")
+    __DB_PATH = os.path.join(__BASE_DIR, "geb_arai_dee.db")
 
     def __init__(self):
-        self.connection = sqlite3.connect(AppDAO.__db_path)
+        self.connection = sqlite3.connect(AppDAO.__DB_PATH)
         self.user_dao = UserDAO(self.connection)
-        self.customer_dao = CustomerDAO()
-        self.room_dao = RoomDAO()
+        self.customer_dao = CustomerDAO(self.connection)
+        self.product_dao = ProductDAO()
         self.shelf_dao = ShelfDAO()
-        self.log_dao = LogDAO()
+        self.log_dao = LogDAO(self.connection)
 
     def get_all_users(self) -> list[User]:
         return self.user_dao.get_all_users()
@@ -30,13 +31,8 @@ class AppDAO:
     def get_user_by_username(self, username: str) -> User:
         return self.user_dao.get_user_by_username(username)
 
+    def insert_log_entry(self, log_entry: LogEntry):
+        self.log_dao.add_log_entry(log_entry)
+
     def close_db(self):
         self.connection.close()
-
-
-if __name__ == "__main__":
-    app_dao = AppDAO()
-    user = app_dao.get_user_by_username("test")
-    print(
-        f"ID: {user.get_id()}, USERNAME: {user.get_username()}, PASS: {user.get_password()}")
-    app_dao.close_db()

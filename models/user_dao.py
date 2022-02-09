d6e129c4c4f145a3e53d58e9d253de77c016a4c7
deeckn <dee28511@gmail.com>
2022-02-09 11:46:36 +0700

    Refactor conditionals and add static constants

--- models/user_dao.py
+++ models/user_dao.py
@@ -6,55 +6,60 @@ from user import User
 class UserDAO:
 
     __table_name = "USERS"
-    __query_list = list()
+    __COLUMN_ID = "id"
+    __COLUMN_FIRST_NAME = "first_name"
+    __COLUMN_LAST_NAME = "last_name"
+    __COLUMN_USERNAME = "username"
+    __COLUMN_PASSWORD = "password"
+    __COLUMN_ACCESS = "access_level"
 
     def __init__(self, connection: sqlite3.Connection):
         self.__connection = connection
         self.__cursor = self.__connection.cursor()
+        self.__query_list = list()
 
     def get_all_users(self) -> list[User]:
+        """Retrieves all users from the USERS table"""
         self.__cursor.execute(f"SELECT * FROM {UserDAO.__table_name}")
         users = self.__cursor.fetchall()
         self.__query_list = users
         self.__convert_to_object()
         return self.__query_list
 
-    def __convert_to_object(self):
+    def __convert_to_object(self) -> None:
+        """Converts raw data to User objects"""
         temp = list()
         for data in self.__query_list:
-            if data[3] == "admin":
-                access = AdminAccess()
-            else:
-                access = EmployeeAccess()
-
-            user = User(data[0], data[1], data[2], access)
+            access = AdminAccess() if data[5] == "admin" else EmployeeAccess()
+            user = User(data[0], data[1], data[2], data[3], data[4], access)
             temp.append(user)
         self.__query_list = temp
 
     def get_user_by_id(self, id: int) -> User:
+        """Retreives a User object from an id"""
         self.__cursor.execute(
-            f"SELECT * FROM {UserDAO.__table_name} WHERE id={id}")
+            f"SELECT * FROM {UserDAO.__table_name} WHERE {UserDAO.__COLUMN_ID}={id}")
         data = self.__cursor.fetchone()
         if data is None:
             return None
-        else:
-            if data[3] == "admin":
-                access = AdminAccess()
-            else:
-                access = EmployeeAccess()
 
-            return User(data[0], data[1], data[2], access)
+        access = AdminAccess() if data[5] == "admin" else EmployeeAccess()
+        return User(data[0], data[1], data[2], data[3], data[4], access)
 
     def get_user_by_username(self, username: str) -> User:
+        """Retreives a User object from a username"""
         self.__cursor.execute(
-            f"SELECT * FROM {UserDAO.__table_name} WHERE username='{username}'")
+            f"SELECT * FROM {UserDAO.__table_name} WHERE {UserDAO.__COLUMN_USERNAME}='{username}'")
         data = self.__cursor.fetchone()
         if data is None:
             return None
-        else:
-            if data[3] == "admin":
-                access = AdminAccess()
-            else:
-                access = EmployeeAccess()
 
-            return User(data[0], data[1], data[2], access)
+        access = AdminAccess() if data[5] == "admin" else EmployeeAccess()
+        return User(data[0], data[1], data[2], data[3], data[4], access)
+
+    def update_user(self, user: User) -> None:
+        """Updates the data of the given user"""
+        pass
+
+    def delete_user_by_username(self, username: str) -> None:
+        pass

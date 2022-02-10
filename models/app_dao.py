a6332ca28dee043a5980872e8711cccca1dfe5a9
deeckn <dee28511@gmail.com>
2022-02-10 15:47:08 +0700

    Insert docstrings

--- models/app_dao.py
+++ models/app_dao.py
@@ -22,17 +22,34 @@ class AppDAO:
         self.shelf_dao = ShelfDAO()
         self.log_dao = LogDAO(self.connection)
 
+    """User Data Access CRUD Methods"""
+
     def get_all_users(self) -> list[User]:
+        """Returns a list of User objects from the database"""
         return self.user_dao.get_all_users()
 
     def get_user_by_id(self, id: int) -> User:
+        """Returns a User object given by an id"""
         return self.user_dao.get_user_by_id(id)
 
     def get_user_by_username(self, username: str) -> User:
+        """Returns a User object given by a username"""
         return self.user_dao.get_user_by_username(username)
 
+    def insert_user(self, user: User):
+        """Inserts a user into the database"""
+        self.user_dao.add_user(user)
+
+    """Log Data Access CRUD Methods"""
+
     def insert_log_entry(self, log_entry: LogEntry):
+        """Inserts a log entry to the database"""
         self.log_dao.add_log_entry(log_entry)
 
+    def get_all_log_entries(self) -> list[LogEntry]:
+        """Returns a list of log entries from the database"""
+        return self.log_dao.get_all_log_entries()
+
     def close_db(self):
+        """Disconnects the connection to the database"""
         self.connection.close()

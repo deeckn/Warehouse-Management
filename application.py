317241d257b89cc969aee466b0a3c5a2baeaa656
deeckn <dee28511@gmail.com>
2022-02-12 16:34:03 +0700

    Fix import from restructure

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from controllers.login_page import LoginPage
+from controllers import *
 from views.forms.login_view import LoginView
-from models.login_model import LoginModel
+from models import LoginModel
 
 
 class Application:

7ff56286176c986731a8b0d6867ca881a83343b5
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-02-24 15:53:00 +0700

    Create Theme static class for consistency

---
+++ views/theme.py
@@ -0,0 +1,50 @@
+from PySide6.QtGui import QFont
+
+
+class Theme:
+    # TYPOGRAPHY
+    POPPINS_REGULAR_72 = QFont("Poppins")
+    POPPINS_REGULAR_72.setPixelSize(72)
+
+    POPPINS_REGULAR_64 = QFont("Poppins")
+    POPPINS_REGULAR_64.setPixelSize(64)
+
+    POPPINS_REGULAR_48 = QFont("Poppins")
+    POPPINS_REGULAR_48.setPixelSize(48)
+
+    POPPINS_REGULAR_36 = QFont("Poppins")
+    POPPINS_REGULAR_36.setPixelSize(36)
+
+    POPPINS_REGULAR_24 = QFont("Poppins")
+    POPPINS_REGULAR_24.setPixelSize(24)
+
+    POPPINS_BOLD_72 = QFont("Poppins")
+    POPPINS_BOLD_72.setPixelSize(72)
+    POPPINS_BOLD_72.setBold(True)
+
+    POPPINS_BOLD_64 = QFont("Poppins")
+    POPPINS_BOLD_64.setPixelSize(64)
+    POPPINS_BOLD_64.setBold(True)
+
+    POPPINS_BOLD_48 = QFont("Poppins")
+    POPPINS_BOLD_48.setPixelSize(48)
+    POPPINS_BOLD_48.setBold(True)
+
+    POPPINS_BOLD_36 = QFont("Poppins")
+    POPPINS_BOLD_36.setPixelSize(36)
+    POPPINS_BOLD_36.setBold(True)
+
+    POPPINS_BOLD_24 = QFont("Poppins")
+    POPPINS_BOLD_24.setPixelSize(24)
+    POPPINS_BOLD_24.setBold(True)
+
+    # COLORS
+    BLACK = "000000"
+    GREY = "DDDDDD"
+    GHOST_WHITE = "#F8F8FF"
+    WHITE = "FFFFFF"
+    DARK_BLUE = "1A374D"
+    BLUE = "406882"
+    YELLOW = "F3CF48"
+    RED = "FF7474"
+    GREEN = "2ACAB0"

90947ba94cb0bd16dcbabac93f207fe5ff6c9e20
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2022-03-01 09:08:22 +0700

    Refactor LogItem

--- views/items/log_item.py
+++ views/items/log_item.py
@@ -1,28 +1,37 @@
 from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout
-from PySide6.QtGui import QFont, Qt
+from PySide6.QtGui import Qt
+from views.theme import Theme
+from data.data_classes import LogEntry
+
 
 class LogItem(QWidget):
-    def __init__(self,date: str, time:str, txt: str):
-        font = QFont("Poppins")
-        font.setPixelSize(14)
+    def __init__(self, log_entry: LogEntry):
         QWidget.__init__(self, None)
-        self.setFont(font)
-        self.main_layout = QHBoxLayout(self)
-        self.main_layout.setSpacing(0)
-        self.main_layout.setContentsMargins(0,0,0,0)
-        self.date_label = QLabel(date)
-        self.date_label.setFixedHeight(21)
-        self.time_label  = QLabel(time)
-        self.time_label.setFixedHeight(21)
-        self.event_label = QLabel(txt)
-        self.event_label.setMinimumWidth(180)
-        self.event_label.setWordWrap(True) # Able the label to be multi-line
+
+        main_layout = QHBoxLayout(self)
+        main_layout.setSpacing(0)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.log_entry = log_entry
+
+        date_label = QLabel(log_entry.get_date())
+        time_label = QLabel(log_entry.get_time())
+        event_label = QLabel(log_entry.get_description())
+
+        for label in (date_label, time_label, event_label):
+            label.setStyleSheet("background-color: none;")
+            label.setFont(Theme.POPPINS_REGULAR_14)
+
+        date_label.setFixedHeight(21)
+        time_label.setFixedHeight(21)
+        event_label.setMinimumWidth(180)
+        event_label.setWordWrap(True)  # Able the label to be multi-line
 
         # Choose
-        self.event_label.setAlignment(Qt.AlignLeft)
-        #self.event_label.setAlignment(Qt.AlignCenter)
+        event_label.setAlignment(Qt.AlignLeft)
+        # self.event_label.setAlignment(Qt.AlignCenter)
 
         # Add Labels to layout
-        self.main_layout.addWidget(self.date_label)
-        self.main_layout.addWidget(self.time_label)
-        self.main_layout.addWidget(self.event_label)
+        main_layout.addWidget(date_label)
+        main_layout.addWidget(time_label)
+        main_layout.addWidget(event_label)

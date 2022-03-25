c23801449b1fae378b2cadfd2db498c0eed14d2c
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2022-03-25 12:01:41 +0700

    Delete customer_list_view.py

--- views/forms/customer_list_view.py
+++
@@ -1,7 +0,0 @@
-from views.forms.stack_page import StackPage  # For real test
-from views.theme import Theme
-
-
-class CustomerListView(StackPage):
-    def __init__(self) -> None:
-        super().__init__()

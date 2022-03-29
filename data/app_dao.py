8ef494da8adeda986d1e0333a40414c814fb74f9
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-03-29 21:54:11 +0700

    Add methods to check existance of database objects

--- data/app_dao.py
+++ data/app_dao.py
@@ -111,6 +111,9 @@ class UserDAO(DAO):
 
     def add_user(self, user: User) -> None:
         """Adds a User object to the database"""
+        if self.exist(user):
+            return
+
         access = "admin" if isinstance(
             user.get_access_level(), AdminAccess) else "employee"
         self.cursor.execute(f"""
@@ -128,6 +131,24 @@ class UserDAO(DAO):
             '{access}')
         """)
         self.connection.commit()
+
+    def exist(self, user: User) -> bool:
+        """Returns True if the User exist in the database"""
+        access = "admin" if isinstance(
+            user.get_access_level(), AdminAccess) else "employee"
+
+        self.cursor.execute(f"""
+            SELECT * FROM {UserDAO.__table_name}
+            WHERE {UserDAO.__COLUMN_FIRST_NAME}='{user.get_first_name()}'
+            AND {UserDAO.__COLUMN_LAST_NAME}='{user.get_last_name()}'
+            AND {UserDAO.__COLUMN_USERNAME}='{user.get_username()}'
+            AND {UserDAO.__COLUMN_PASSWORD}='{user.get_password()}'
+            AND {UserDAO.__COLUMN_ACCESS}='{access}'
+        """)
+
+        if self.cursor.fetchone() is None:
+            return False
+        return True
 
     def update_user(
         self,
@@ -268,6 +289,9 @@ class CustomerDAO(DAO):
 
     def add_customer(self, customer: Customer):
         """Adds a Customer object to the database"""
+        if self.exist(customer):
+            return
+
         packing_service = 1 if customer.get_packing_service() else 0
 
         self.cursor.execute(f"""
@@ -368,6 +392,26 @@ class CustomerDAO(DAO):
 
         self.__convert_to_object()
         return self.__query_list
+
+    def exist(self, customer: Customer) -> bool:
+        """Returns True if a customer exist in the database"""
+        packing_service = 1 if customer.get_packing_service() else 0
+
+        self.cursor.execute(f"""
+            SELECT * FROM {CustomerDAO.__table_name}
+            WHERE {CustomerDAO.__COLUMN_NAME}='{customer.get_name()}'
+            AND {CustomerDAO.__COLUMN_PHONE}='{customer.get_phone()}'
+            AND {CustomerDAO.__COLUMN_EMAIL}='{customer.get_email()}'
+            AND {CustomerDAO.__COLUMN_PACKING_SERVICE}={packing_service}
+            AND {CustomerDAO.__COLUMN_RENTAL_DURATION}='{customer.get_rental_duration()}'
+            AND {CustomerDAO.__COLUMN_DATE_JOINED}='{customer.get_date_joined()}'
+            AND {CustomerDAO.__COLUMN_EXPIRY_DATE}='{customer.get_expiry_date()}'
+            AND {CustomerDAO.__COLUMN_TOTAL_PAYMENT}={customer.get_total_payment()}
+        """)
+
+        if self.cursor.fetchone() is None:
+            return False
+        return True
 
     def delete_customer(self, customer_id: int):
         """Deletes customer data given an id"""
@@ -526,6 +570,8 @@ class ProductDAO(DAO):
 
     def add_product(self, product: ProductItem):
         """Adds a Product object to the PRODUCTS table"""
+        if self.exist(product):
+            return
 
         try:
             self.cursor.execute(f"""
@@ -569,6 +615,22 @@ class ProductDAO(DAO):
                 the there might be a product with same id"
             )
             return
+
+    def exist(self, product: ProductItem) -> bool:
+        """Returns True if a Product exist in the database"""
+        self.cursor.execute(f"""
+            SELECT * FROM {ProductDAO.__table_name}
+            WHERE {ProductDAO.__COLUMN_NAME}='{product.get_name()}'
+            AND {ProductDAO.__COLUMN_QUANTITY}={product.get_quantity()}
+            AND {ProductDAO.__COLUMN_LOW_STOCK}={product.get_low_stock_quantity()}
+            AND {ProductDAO.__COLUMN_WEIGHT}={product.get_weight()}
+            AND {ProductDAO.__COLUMN_LAST_STORED}='{product.get_last_stored()}'
+            AND {ProductDAO.__COLUMN_OWNER}={product.get_owner().get_id()}
+        """)
+
+        if self.cursor.fetchone() is None:
+            return False
+        return True
 
     def get_product(self, product_id: int) -> ProductItem:
         """Returns a ProductItem object given an id"""
@@ -800,6 +862,9 @@ class ShelfDAO(DAO):
 
     def add_shelf(self, shelf: StorageShelf):
         """Add shelf object to the STORAGE_SHELF table"""
+        if self.exist(shelf):
+            return
+
         try:
             self.cursor.execute(f"""
                 INSERT INTO {ShelfDAO.__table_name}
@@ -827,6 +892,23 @@ class ShelfDAO(DAO):
                 the there might be a shelf with same label"
             )
             return
+
+    def exist(self, shelf: StorageShelf) -> bool:
+        """Returns True if a Shelf exist in the database"""
+        self.cursor.execute(f"""
+            SELECT * FROM {ShelfDAO.__table_name}
+            WHERE {ShelfDAO.__COLUMN_LABEL}='{shelf.get_label()}'
+            AND {ShelfDAO.__COLUMN_MAX_WEIGHT}={shelf.get_max_weight()}
+            AND {ShelfDAO.__COLUMN_LENGTH}={shelf.get_length()}
+            AND {ShelfDAO.__COLUMN_WIDTH}={shelf.get_width()}
+            AND {ShelfDAO.__COLUMN_HEIGHT}={shelf.get_height()}
+            AND {ShelfDAO.__COLUMN_ROWS}={shelf.get_rows()}
+            AND {ShelfDAO.__COLUMN_COLUMNS}={shelf.get_columns()}
+        """)
+
+        if self.cursor.fetchone() is None:
+            return False
+        return True
 
     def get_shelf_by_label(self, shelf_label: str) -> StorageShelf:
         """Returns a StorageShelf object given a label"""

68ae7cf9fc01a6e7c122f0a1600138839c6c0847
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-04-02 11:10:32 +0700

    Database retrieval validation

--- controllers.py
+++ controllers.py
@@ -80,6 +80,10 @@ class AccountPage(Controller):
     def fill_user_cards(self):
         """Retreives a list of user and display as cards"""
         employees = self.model.get_employee_accounts()
+
+        if employees is None:
+            return
+
         for user in employees:
             self.view.add_employee_card(user)
 
@@ -200,6 +204,10 @@ class NotificationPage(Controller):
             )
 
         customers = self.model.get_contract_ending_customers()
+
+        if customer is None:
+            return
+
         for customer in customers:
             ending_days = self.model.date_difference(customer)
             self.view.add_event_card(
@@ -209,6 +217,7 @@ class NotificationPage(Controller):
                 str(customer.get_id())
             )
 
+
 class InventoryOverviewPage(Controller):
     view: InventoryOverviewView
     model: InventoryOverviewModel
@@ -216,14 +225,19 @@ class InventoryOverviewPage(Controller):
     def __init__(self, view: QWidget, model: Model):
         super().__init__(view, model)
         self.__fill_customer_list()
-        self.view.set_customer_selected_function(self.fill_selected_customer_products)
-        
+        self.view.set_customer_selected_function(
+            self.fill_selected_customer_products)
+
     def __fill_customer_list(self):
-        self.customer = self.model.get_customer_selection()
-        for i in self.customer:
-            self.view.add_customer_item(i.get_name(), self.get_customer_percent_stocked(i.get_id()), i.get_id())
-    
-    def get_customer_percent_stocked(self, id: int)->float:
+        customers = self.model.get_customer_selection()
+        for customer in customers:
+            self.view.add_customer_item(
+                customer.get_name(),
+                self.get_customer_percent_stocked(customer.get_id()),
+                customer.get_id()
+            )
+
+    def get_customer_percent_stocked(self, id: int) -> float:
         return self.model.get_product_stock(id)
 
     def get_products_of_customer(self, id: int) -> list[ProductItem]:
@@ -231,9 +245,12 @@ class InventoryOverviewPage(Controller):
 
     def fill_selected_customer_products(self):
         self.view.clear_product_item()
-        if(self.view.current_customer != None):
-            id = self.view.current_customer.get_customer_id()
-            products = self.get_products_of_customer(id)
-            if(products is not None):
-                for product in products:
-                    self.view.add_product_item(product)
+
+        if self.view.current_customer is None:
+            return
+
+        id = self.view.current_customer.get_customer_id()
+        products = self.get_products_of_customer(id)
+        if products is not None:
+            for product in products:
+                self.view.add_product_item(product)

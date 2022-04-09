a40364760f23d1f14dda7d708c9b4eef07259a64
Dee Deesit <40157947+deeckn@users.noreply.github.com>
2022-04-09 11:23:24 +0700

    Tester file

--- tester.py
+++ tester.py
@@ -1,10 +1,9 @@
-from data.app_dao import AppDAO, ProductDAO
 from views.root_container import RootContainer
 from PySide6.QtWidgets import QApplication
 import sys
 
 if __name__ == "__main__":
-    dao: ProductDAO = AppDAO.get_dao("product")
-    product = dao.get_product(1)
-    print(product)
-    AppDAO.close_db()
+    app = QApplication(sys.argv)
+    view = RootContainer()
+    view.showFullScreen()
+    sys.exit(app.exec())

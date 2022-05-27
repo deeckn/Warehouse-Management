d0c2b475689c22dc2ecbdff0003351168719f156
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2022-05-27 17:35:54 +0700

    Rename method and add comment

--- views/forms/location_page_view.py
+++ views/forms/location_page_view.py
@@ -2,15 +2,14 @@ from cgitb import reset
 from PySide6.QtWidgets import QComboBox, QPushButton, QLabel, QWidget, QGridLayout
 from data.orm.schema import Shelf
 from views.theme import Theme
-from PySide6.QtCore import QRect, QSize
-from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QRect
 
 
 class LocationPageView(QWidget):
     def __init__(self, parent=None) -> None:
         QWidget.__init__(self, parent)
         self.current_position = None
-        self.avail_shelfs = None
+        self.avail_shelves = None
         self.current_shelf = None
 
         bg = QLabel(self)
@@ -21,18 +20,6 @@ class LocationPageView(QWidget):
         self.shelf_select_cb.setGeometry(QRect(610, 65, 300, 50))
         self.shelf_select_cb.setFont(Theme.POPPINS_BOLD_24)
 
-        self.next_shelf_bt = QPushButton(parent=self)
-        self.next_shelf_bt.move(1310, 65)
-        self.next_shelf_bt.setIcon(QPixmap(f":/icons/next_btn.png"))
-        self.next_shelf_bt.setIconSize(QSize(40, 40))
-        self.next_shelf_bt.setStyleSheet("background-color: transparent;")
-
-        self.back_shelf_bt = QPushButton(parent=self)
-        self.back_shelf_bt.move(165, 65)
-        self.back_shelf_bt.setIcon(QPixmap(f":/icons/back_btn.png"))
-        self.back_shelf_bt.setIconSize(QSize(40, 40))
-        self.back_shelf_bt.setStyleSheet("background-color: transparent;")
-
         self.back_bt = QPushButton(parent=self, text="Back")
         self.back_bt.setGeometry(160, 950, 111, 52)
         self.back_bt.setFont(Theme.POPPINS_BOLD_24)
@@ -42,8 +29,7 @@ class LocationPageView(QWidget):
         self.confirm_bt = QPushButton(parent=self, text="CONFIRM")
         self.confirm_bt.setGeometry(1187, 950, 173, 53)
         self.confirm_bt.setFont(Theme.POPPINS_BOLD_24)
-        self.confirm_bt.setStyleSheet(
-            f"background-color:{Theme.YELLOW};color:white;border-radius:15px")
+        self.set_enable_confirm(False)
 
         table_bg = QLabel(self)
         table_bg.setGeometry(160, 210, 1200, 700)
@@ -82,14 +68,6 @@ class LocationPageView(QWidget):
         ui_yellow_indicator.setFont(Theme.POPPINS_REGULAR_18)
         ui_yellow_indicator.move(490, 159)
 
-    def set_up(self, available_shelfs: list[Shelf]):
-        self.avail_shelfs = available_shelfs
-
-        for storage_shelf in available_shelfs:
-            self.shelf_select_cb.addItem(storage_shelf.get_label())
-        self.current_shelf = available_shelfs[0]
-        self.update_table()
-
     def fill_occupied(self, occupied_slots: list[int]):
         buttons = self.table_widget.children()
         for i in occupied_slots:
@@ -116,10 +94,12 @@ class LocationPageView(QWidget):
         if(self.current_position == bt):
             self.clear_current_pos()
             self.current_position = None
+            self.set_enable_confirm(False)
             return
 
         if(self.current_position != None):
             self.clear_current_pos()
+        self.set_enable_confirm(True)
         self.current_position = bt
         bt.setStyleSheet(
             f"background-color: {Theme.YELLOW}; color: {Theme.GHOST_WHITE}; border:none;")
@@ -144,9 +124,6 @@ class LocationPageView(QWidget):
         self.current_position.setStyleSheet(
             f"background-color: {Theme.YELLOW}; color: {Theme.GHOST_WHITE}; border:none;")
 
-    def get_current_shelf(self) -> int:
-        return self.shelf_select_cb.currentIndex()
-
     def set_event_change_shelf(self, function):
         self.shelf_select_cb.currentIndexChanged.connect(function)
 
@@ -154,7 +131,7 @@ class LocationPageView(QWidget):
         return self.shelf_select_cb.currentText()
 
     def update_shelf(self):
-        self.current_shelf = self.avail_shelfs[self.shelf_select_cb.currentIndex(
+        self.current_shelf = self.avail_shelves[self.shelf_select_cb.currentIndex(
         )]
 
     def update_table(self):
@@ -174,3 +151,27 @@ class LocationPageView(QWidget):
             button.setFont(Theme.POPPINS_BOLD_16)
             button.clicked.connect(self.select_position)
             self.grid.addWidget(button, *position)
+
+    def rerender_page(self, new_shelves: list[Shelf]):
+        """Use for rerender page after click locatioin button"""
+        # Clear past page info
+        self.reset_table()
+        self.reset_shelf()
+        # Assign new shelf to avial_shelves attr
+        self.avail_shelves = new_shelves
+        # Loop all shelf in new_shelves
+        for storage_shelf in new_shelves:
+            # Add Each shelf label to combo box
+            self.shelf_select_cb.addItem(storage_shelf.get_label())
+        # Assign the current shelf as the first shelf in new shelves
+        self.current_shelf = new_shelves[0]
+        self.update_table()  # Update the tabel according to the current shelf attr
+
+    def set_enable_confirm(self, boolean: bool):
+        self.confirm_bt.setEnabled(boolean)
+        self.confirm_bt.setStyleSheet(
+            f"background-color: {Theme.YELLOW if boolean else Theme.DARK_YELLOW}; color:{'white' if boolean else Theme.DARK_WHITE}; border-radius: 15px;")
+
+    def set_current_shelf(self, label: str):
+        i = self.shelf_select_cb.findText(label)
+        self.shelf_select_cb.setCurrentIndex(i)
